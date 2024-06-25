df482cb07a6c5a0918a6bf4ab436b6ca7f4c2b6c
Abdul Rehman <abrehman4163@gmail.com>
2024-06-25 08:35:40 +0500

    Main screen completed

--- lib/main.dart
+++ lib/main.dart
@@ -14,15 +14,101 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.only(top: 80.0),
+        padding: const EdgeInsets.only(top: 80.0,left: 30),
         child: Column(
           children: <Widget>[
             Row(children: <Widget>[
               CircleAvatar(radius: 50,backgroundColor: Colors.pink,),
-              Column(children: <Widget>[
-                Text("Name"),Text("Role")
-              ],)
-            ],)
+              Padding(
+                padding: const EdgeInsets.only(left: 28.0),
+                child: Column(
+
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                  Text("Name",style: TextStyle(fontSize: 24),),
+                  Text("Role",style: TextStyle(fontSize: 18),)
+                ],),
+              )
+            ]),
+            SizedBox(height: 30,),
+            Padding(
+              padding: const EdgeInsets.only(left: 15),
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(right: 8.0),
+                          child: Icon(Icons.school),
+                        ),
+                        Text("National University of Science and Technology"),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(right: 8.0),
+                          child: Icon(Icons.work),
+                        ),
+                        Text("5+ Projects"),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(right: 8.0),
+                          child: Icon(Icons.location_on_rounded),
+                        ),
+                        Text("H-12 Islamabad"),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(right: 8.0),
+                          child: Icon(Icons.mail),
+                        ),
+                        Text("abrehman4163@gmail.com"),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(right:8.0),
+                          child: Icon(Icons.phone),
+                        ),
+                        Text("030249*****"),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 30.0),
+              child: Column(
+                children: <Widget>[
+                  Text("Developed by",style: TextStyle(fontSize: 16),),
+                  Text("Abdul RehmanS",style: TextStyle(fontSize: 12),),
+                ],
+              ),
+            )
+
+
           ],
         ),
       ),
