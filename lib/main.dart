56e35bec7222bb43ae659e57a3324c585b159877
Abdul Rehman <abrehman4163@gmail.com>
2024-06-26 04:25:59 +0500

    Portfolio completed

--- lib/main.dart
+++ lib/main.dart
@@ -13,103 +13,130 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(top: 80.0,left: 30),
-        child: Column(
-          children: <Widget>[
-            Row(children: <Widget>[
-              CircleAvatar(radius: 50,backgroundColor: Colors.pink,),
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("Images/bg.jfif"),
+              fit: BoxFit.cover
+            )),
+
+        child: Padding(
+          padding: const EdgeInsets.only(top: 80.0,left: 30),
+          child: Column(
+            children: <Widget>[
+              Row(children: <Widget>[
+                CircleAvatar(radius: 50,backgroundColor: Colors.pink,
+                  backgroundImage: AssetImage("Images/image.jpeg"),),
+                Padding(
+                  padding: const EdgeInsets.only(left: 28.0),
+                  child: Column(
+
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                    Text("Abdul Rehman",style: TextStyle(fontSize: 24,color:
+                  Colors.white),),
+                    Text("Role",style: TextStyle(fontSize: 18,color:
+                    Colors.white),)
+                  ],),
+                )
+              ]),
+              SizedBox(height: 30,),
               Padding(
-                padding: const EdgeInsets.only(left: 28.0),
+                padding: const EdgeInsets.only(left: 15),
                 child: Column(
-
-                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                  Text("Name",style: TextStyle(fontSize: 24),),
-                  Text("Role",style: TextStyle(fontSize: 18),)
-                ],),
-              )
-            ]),
-            SizedBox(height: 30,),
-            Padding(
-              padding: const EdgeInsets.only(left: 15),
-              child: Column(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.only(right: 8.0),
-                          child: Icon(Icons.school),
-                        ),
-                        Text("National University of Science and Technology"),
-                      ],
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(right: 8.0),
+                            child: Icon(Icons.school,color: Colors.white,size:
+                            30,),
+                          ),
+                          Text("National University of Science and "
+                              "Technology",style: TextStyle(color:
+                          Colors.white),),
+                        ],
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.only(right: 8.0),
-                          child: Icon(Icons.work),
-                        ),
-                        Text("5+ Projects"),
-                      ],
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(right: 8.0),
+                            child: Icon(Icons.work,color: Colors.white,size:
+                            30,),
+                          ),
+                          Text("5+ Projects",style: TextStyle(color:
+                          Colors.white),),
+                        ],
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.only(right: 8.0),
-                          child: Icon(Icons.location_on_rounded),
-                        ),
-                        Text("H-12 Islamabad"),
-                      ],
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(right: 8.0),
+                            child: Icon(Icons.location_on_rounded,color: Colors.white,size:
+                            30,),
+                          ),
+                          Text("H-12 Islamabad",style: TextStyle(color:
+                          Colors.white),),
+                        ],
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.only(right: 8.0),
-                          child: Icon(Icons.mail),
-                        ),
-                        Text("abrehman4163@gmail.com"),
-                      ],
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(right: 8.0),
+                            child: Icon(Icons.mail,color: Colors.white,size:
+                            30,),
+                          ),
+                          Text("abrehman4163@gmail.com",style: TextStyle(color:
+                          Colors.white),),
+                        ],
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.only(right:8.0),
-                          child: Icon(Icons.phone),
-                        ),
-                        Text("030249*****"),
-                      ],
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(right:8.0),
+                            child: Icon(Icons.phone,color: Colors.white,size:
+                            30,),
+                          ),
+                          Text("030249*****",style: TextStyle(color:
+                          Colors.white),),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 30.0),
-              child: Column(
-                children: <Widget>[
-                  Text("Developed by",style: TextStyle(fontSize: 16),),
-                  Text("Abdul RehmanS",style: TextStyle(fontSize: 12),),
-                ],
+              SizedBox(height: 50,),
+              Text("I am MERN Stack and App Developer" ,style: TextStyle
+                (fontSize: 18,color:
+              Colors.white),),
+              SizedBox(height: 250,),
+              Padding(
+                padding: const EdgeInsets.only(top: 30.0),
+                child: Column(
+                  children: <Widget>[
+                    Text("Developed by",style: TextStyle(fontSize: 18,color:
+                    Colors.white),),
+                    Text("Abdul RehmanS",style: TextStyle(fontSize: 14,color:
+                    Colors.white),),
+                  ],
+                ),
               ),
-            )
-
-
-          ],
+            ],
+          ),
         ),
       ),
     );
